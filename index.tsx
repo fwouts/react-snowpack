04ec6a565e55cb497797d01c9693d7588b6d7267
François Wouts <f@zenc.io>
2021-03-18 12:12:47 +1100

    Enable fast refresh

--- index.tsx
+++ index.tsx
@@ -5,5 +5,17 @@ import { App } from "./components/App";
 let i = 0;
 setInterval(() => {
   i += 1;
-  ReactDOM.render(<App count={i} />, document.getElementById("root"));
+  ReactDOM.render(
+    <React.StrictMode>
+      <App count={i} />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
 }, 10);
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+const meta = import.meta as any;
+if (meta.hot) {
+  meta.hot.accept();
+}
